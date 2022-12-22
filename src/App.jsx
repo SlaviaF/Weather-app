d60ea65dc1814c512219b299c63b2cf1790178c1
SlaviaF <sf@exitplanner.app>
2022-12-22 14:01:57 +0100

    fix margin

--- src/App.jsx
+++ src/App.jsx
@@ -79,9 +79,9 @@ function App() {
 
   }
   return (
-    <div className='h-screen w-full' >
+    <div className='h-screen w-full ' >
       <div className='bg-hero bg-cover bg-center w-full h-full flex flex-col items-center justify-center '>
-        <form className='max-h-14 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8'>
+        <form className='max-h-14 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] my-4 sm:my-8'>
           <div className='h-full relative flex items-center justify-between p-2'>
             <input ref={inputRef} onChange={handleInput} className='flex-1 bg-transparent outline-none placeholder:text-white text-white text-sm font-light pl-6 h-full'
               type='text' placeholder='Seach by city or country' />
